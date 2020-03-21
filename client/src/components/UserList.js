499978134a50dd057d66e9b30e5a9980c43b0c4f
rabah babaci <4rabah@gmail.com>
2020-03-21 10:50:33 -0700

    add a darkMode functionality

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -7,10 +7,18 @@ export default function UserList({ userList }) {
   const [isOnDarkMode, switchDarkMode] = useDarkMode(false);
 
   return (
-    <div className='list-wrapper'>
-      {userList.map((user, index) => (
-        <UserCard user={user} key={index} />
-      ))}
+    <div className='userList-container'>
+      <div className='dark-mode__toggle' onClick={switchDarkMode}>
+        <div className={isOnDarkMode ? 'toggle toggled' : 'toggle'} />
+      </div>
+
+      <span id='dark-mode-text'>dark mode</span>
+
+      <div className='list-wrapper'>
+        {userList.map((user, index) => (
+          <UserCard user={user} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
