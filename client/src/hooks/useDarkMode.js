8d5e05c939a6db63e628b80efd6c2bc96cb0c465
rabah babaci <4rabah@gmail.com>
2020-03-21 10:07:16 -0700

    get useDarkMode to add a dark-mode class to the body onToggle

--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-export default function useDarkMode() {
-  return <div></div>;
+export default function useDarkMode({ initialValue }) {
+  const [isOnDarkMode, setIsOnDarkMode] = useState(initialValue);
+
+  useEffect(() => {
+    isOnDarkMode
+      ? document.body.classList.add('dark-mode')
+      : document.body.classList.remove('dark-mode');
+  }, [isOnDarkMode]);
+
+  const turnDarkModeOn = () => setIsOnDarkMode(!isOnDarkMode);
+
+  return [isOnDarkMode, turnDarkModeOn];
 }
