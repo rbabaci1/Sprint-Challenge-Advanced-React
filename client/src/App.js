e1fa9b17726bb56a821bfd45be372fe8c9b9c67b
rabah babaci <4rabah@gmail.com>
2020-03-21 09:35:55 -0700

    add state to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  state = {
+    data: []
+  };
+
   render() {
-    return <div></div>;
+    return <div className='App'></div>;
   }
 }
